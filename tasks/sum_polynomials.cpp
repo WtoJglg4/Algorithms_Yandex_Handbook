9ca9587c6bf428c6e790f65f70c549606c673415
WtoJglg4 <glazovv81@gmail.com>
2023-07-27 20:25:17 +0300

    Solved simple task B from test tasks

--- tasks/sum_polynomials.cpp
+++ tasks/sum_polynomials.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int Sum_polynomials(){
-    //здесь будет функция :)
+const int len = 10;
+
+//int Sum_polynomials(){
+int* Sum_polynomials(int& n, int& m, int* A, int* B){
+  
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        cin >> A[i];
+    }
+    cin>>m;
+    for(int i=0; i<=m; i++){
+        cin >> B[i];
+    }
+    int k = abs(n-m);
+    //cout << "\nk: " << k << endl;
+    if (n >= m){
+        for(int i=k; i<=n + k; i++){
+            A[i] += B[i-k];
+            //cout << A[i] << " ";
+        }
+        return A;
+    }
+    else{
+        for(int i=k; i<=m + k; i++){
+            B[i] += A[i-k];
+            //cout << B[i] << " ";
+        }
+        return B;
+    }
 }
 
 int main(){
-    //
-    cout << Sum_polynomials();
+    int n = 0, m = 0;
+    int A[len] = {101};
+    int B[len] = {101};
+
+    int* arr = Sum_polynomials(n, m, A, B);
+    //cout << arr << endl;
+    int l = max(n, m);
+
+    cout << l << endl;
+    for(int i=0; i<=l; i++){
+        cout << arr[i] << " ";
+    }
 }
